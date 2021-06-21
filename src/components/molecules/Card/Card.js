f7c263cac1a48af5aa86da2eb931c2f2c569774b
PatiDev95 <54117153+PatiDev95@users.noreply.github.com>
2021-06-21 11:18:28 +0200

    Add different types of cards.

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -4,6 +4,7 @@ import Heading from "../../atoms/Heading/Heading";
 import Paragraph from "../../atoms/Paragraph/Paragraph";
 import Button from "../../atoms/Button/Button";
 import PropTypes from "prop-types";
+import LinkIcon from "../../../assets/link.svg";
 
 const StyledWrapper = styled.div`
   min-height: 380px;
@@ -19,6 +20,11 @@ const StyledWrapper = styled.div`
 const InnerWrapper = styled.div`
   padding: 17px 30px;
   background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : "white")};
+  position: relative;
+
+  :first-of-type {
+    z-index: 9999;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -39,12 +45,39 @@ const HeadingStyle = styled(Heading)`
   margin: 0 0 5px;
 `;
 
+const StyledAvatar = styled.img`
+  width: 86px;
+  height: 86px;
+  border: 5px solid ${({ theme }) => theme.twitter};
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  border-radius: 50px;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  width: 47px;
+  height: 47px;
+  border-radius: 50px;
+  background: white url(${LinkIcon}) no-repeat;
+  background-size: 60%;
+  background-position: 50%;
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  transform: translateY(-10%);
+`;
+
 const Card = ({ cardType }) => (
   <>
     <StyledWrapper>
       <InnerWrapper activeColor={cardType}>
         <HeadingStyle>Nagłówek</HeadingStyle>
         <DateInfo>6 dni temu</DateInfo>
+
+        {cardType === "twitter" && <StyledAvatar src="https://unavatar.vercel.app/twitter/hello_roman" />}
+        {cardType === "article" && <StyledLinkButton href="https://youtube.com/helloroman" />}
       </InnerWrapper>
 
       <InnerWrapper flex>
