2170e25978c1ed3e99be7962b629e350d751e07f
PatiDev95 <54117153+PatiDev95@users.noreply.github.com>
2021-06-23 09:45:06 +0200

    Add view details.

---
+++ src/components/templates/DetailsTemplate.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import UserPageTemplate from "./UserPageTemplate";
+
+const DetailsTemplate = ({ children, pageType }) => (
+  <UserPageTemplate pageType={pageType}>
+    {children}
+
+    <Link to="/"> go back</Link>
+  </UserPageTemplate>
+);
+
+export default DetailsTemplate;
